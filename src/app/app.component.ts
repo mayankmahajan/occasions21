08c42c1860e62e6696e3129b8e446b912af683d7
Mayank Mahajan <Mayank.Mahajan@guavus.com>
2018-06-16 01:37:57 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { AdService }         from './ad-banner/ad.service';
+import { AdItem }            from './ad-banner/ad-item';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ads: AdItem[];
+  constructor(private adService: AdService) {}
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { AppComponent }         from './app.component';
+import { HeroJobAdComponent }   from './ad-banner/hero-job-ad.component';
+import { AdBannerComponent }    from './ad-banner/ad-banner.component';
+import { HeroProfileComponent } from './ad-banner/hero-profile.component';
+import { AdDirective }          from './ad-banner/ad.directive';
+import { AdService }            from './ad-banner/ad.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule ],
+  providers: [AdService],
+  declarations: [ AppComponent,
+                  AdBannerComponent,
+                  HeroJobAdComponent,
+                  HeroProfileComponent,
+                  AdDirective ],
+  entryComponents: [ HeroJobAdComponent, HeroProfileComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {}
+}
